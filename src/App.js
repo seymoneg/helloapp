d20c52965a8bfa9793a0f029ea09df13c4f3c1cd
seymoneg <seymonegug@gmail.com>
2023-06-28 22:45:51 -0400

    hello world and time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
 
-function App() {
+  const currDate = new Date();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h2>The time is {currDate.toLocaleTimeString()}</h2>
     </div>
   );
 }
